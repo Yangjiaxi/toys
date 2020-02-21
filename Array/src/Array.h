95c199f44c6946ba1e62c7962c87d6c15e3c4b84
Yangjiaxi <1468764755@qq.com>
2020-02-21 22:44:19 +0800

    <MOD> format

--- Array/src/Array.h
+++ Array/src/Array.h
@@ -25,12 +25,10 @@
  * c++中使用迭代器进行范围处理，而js的Array操作普遍都作用于所有元素，
  * 例如map, forEach方法
  */
-template <class T>
-class Array
-{
+template <class T> class Array {
     using iterator = typename std::vector<T>::iterator;
 
-   public:
+  public:
     std::vector<T> data;
 
     /*
@@ -42,18 +40,18 @@ class Array
             Array(iterator l, iterator r) -> 范围迭代器初始化
 
      */
-   public:
+  public:
     Array() : data() {}
     Array(int length, T init_value) : data(length, init_value) {}
     Array(std::initializer_list<T> il) : data(il) {}
     Array(std::vector<T> v) : data(v) {}
     Array(iterator l, iterator r) : data(l, r) {}
-    Array(const Array& B) : data(B.data) {}
+    Array(const Array &B) : data(B.data) {}
 
     /*
         操作符重载
     */
-    T& operator[](size_t idx) { return data[idx]; }
+    T &operator[](size_t idx) { return data[idx]; }
 
     /*
         方法：
@@ -66,11 +64,11 @@ class Array
 
     // .push(std::initializer_list<T>) -> this
     // 添加多个元素到尾部
-    Array<T>& push(std::initializer_list<T>);
+    Array<T> &push(std::initializer_list<T>);
 
     // .push(T) -> this
     // 添加一个元素到尾部
-    Array<T>& push(T);
+    Array<T> &push(T);
 
     // .shift() -> T
     // 删除第一个元素并返回这个元素
@@ -78,11 +76,11 @@ class Array
 
     // .unshift(std::initializer_list<T>) -> this
     // 添加多个元素到头部
-    Array<T>& unshift(std::initializer_list<T>);
+    Array<T> &unshift(std::initializer_list<T>);
 
     // .unshift(T) -> this
     // 添加一个元素到头部
-    Array<T>& unshift(T);
+    Array<T> &unshift(T);
 
     // .every(std::function<bool(T)>) -> bool
     // 测试所有元素是否都通过了指定函数的测试
@@ -94,7 +92,7 @@ class Array
 
     // .find(std::function<bool(T)>) -> T
     // 返回指向第一个通过测试的元素的拷贝，否则nullptr
-    T* find(std::function<bool(T)>);
+    T *find(std::function<bool(T)>);
 
     // .findIndex(std::function<bool(T)) -> int
     // 返回第一个通过测试的元素的索引，否则-1
@@ -102,49 +100,49 @@ class Array
 
     // .includes(T) -> bool
     // 返回是否包含某个特定值
-    bool includes(T&);
-    bool includes(T&&);
+    bool includes(T &);
+    bool includes(T &&);
 
     // .indexOf(T[, fromIndex=0]) -> int
     // 返回可以找到的第一个给定元素的索引，否则-1
-    int indexOf(T&);
-    int indexOf(T&&);
-    int indexOf(T&, size_t);
-    int indexOf(T&&, size_t);
+    int indexOf(T &);
+    int indexOf(T &&);
+    int indexOf(T &, size_t);
+    int indexOf(T &&, size_t);
 
     // .lastIndexOf(T[, fromIndex=.length-1]) -> int
     // 从末尾开始，返回可以找到的第一个给定元素的索引，否则-1
-    int lastIndexOf(T&);
-    int lastIndexOf(T&&);
-    int lastIndexOf(T&, size_t);
-    int lastIndexOf(T&&, size_t);
+    int lastIndexOf(T &);
+    int lastIndexOf(T &&);
+    int lastIndexOf(T &, size_t);
+    int lastIndexOf(T &&, size_t);
 
     // .copyWithin(target[, start[, end]]) -> this
     // 将[start, end)覆盖到target开始的区域，inplace
-    Array<T>& copyWithin(size_t);
-    Array<T>& copyWithin(size_t, size_t);
-    Array<T>& copyWithin(size_t, size_t, size_t);
+    Array<T> &copyWithin(size_t);
+    Array<T> &copyWithin(size_t, size_t);
+    Array<T> &copyWithin(size_t, size_t, size_t);
 
     // .fill(value[, start[, end]]) -> this
     // 用value填充[start...end-1]，inplace
-    Array<T>& fill(T);
-    Array<T>& fill(T, size_t);
-    Array<T>& fill(T, size_t, size_t);
+    Array<T> &fill(T);
+    Array<T> &fill(T, size_t);
+    Array<T> &fill(T, size_t, size_t);
 
     // .reverse() -> this
     // 倒序Array
-    Array<T>& reverse();
+    Array<T> &reverse();
 
     // .sort(std::function<bool(T, T)>) -> this
     // 原地升序排序
-    Array<T>& sort();
-    Array<T>& sort(std::function<bool(T, T)>);
+    Array<T> &sort();
+    Array<T> &sort(std::function<bool(T, T)>);
 
     // .splice(start[, deleteCount[, {item1...itemN}]]) -> this
     // 从start开始删除deleteCount个元素，然后插入一堆元素，为空则仅删除，原地执行
-    Array<T>& splice(size_t);
-    Array<T>& splice(size_t, size_t);
-    Array<T>& splice(size_t, size_t, std::initializer_list<T>);
+    Array<T> &splice(size_t);
+    Array<T> &splice(size_t, size_t);
+    Array<T> &splice(size_t, size_t, std::initializer_list<T>);
 
     // .from() -> Array<T>
     // 返回浅拷贝副本
@@ -152,7 +150,7 @@ class Array
 
     // .concat(Array<T> B) -> Array<T>
     // 返回A#B，是一个新的Array
-    Array<T> concat(Array<T>& B);
+    Array<T> concat(Array<T> &B);
 
     // .filter(std::function<bool(T)>) -> Array<T>
     // 返回通过测试的元素构成的Array
@@ -160,8 +158,7 @@ class Array
 
     // .map<std::function<U(T)>> -> Array<U>
     // 返回对所有元素应用某个函数后的新Array
-    template <class U>
-    Array<U> map(std::function<U(T)>);
+    template <class U> Array<U> map(std::function<U(T)>);
 
     // .slice([begin[, end]]) -> Array<T>
     // 从begin提取到end，不包含end，默认为[0, n)
@@ -171,13 +168,11 @@ class Array
 
     // .reduce(std::function<U(U, T)>, init_value) -> U
     // 从init_value开始对数组中元素升序执行reduce
-    template <class U>
-    U reduce(std::function<U(U, T)>, U);
+    template <class U> U reduce(std::function<U(U, T)>, U);
 
     // .reduceRight(std::function<T(T, T)>, init_value) -> T
     // 从init_value开始对数组中元素升序执行reduce
-    template <class U>
-    U reduceRight(std::function<U(U, T)>, U);
+    template <class U> U reduceRight(std::function<U(U, T)>, U);
 
     // .forEach(std::function<void(T)>)
     // 对数组中所有元素应用
@@ -186,368 +181,229 @@ class Array
     // .join(spt=',') -> string
     std::string join();
     std::string join(std::string spt);
-    std::ostream& output(std::ostream& os);
+    std::ostream &output(std::ostream &os);
 
     // .size() -> size_t
     // 返回元素个数
     size_t size();
 };
 
-template <class T>
-T Array<T>::pop()
-{
+template <class T> T Array<T>::pop() {
     auto val = data.back();
     data.pop_back();
     return val;
 }
 
-template <class T>
-Array<T>& Array<T>::push(std::initializer_list<T> il)
-{
+template <class T> Array<T> &Array<T>::push(std::initializer_list<T> il) {
     data.insert(data.end(), il);
     return *this;
 }
 
-template <class T>
-Array<T>& Array<T>::push(T e)
-{
+template <class T> Array<T> &Array<T>::push(T e) {
     data.insert(data.end(), e);
     return *this;
 }
 
-template <class T>
-T Array<T>::shift()
-{
+template <class T> T Array<T>::shift() {
     auto val = data.front();
     data.erase(data.begin());
     return val;
 }
 
-template <class T>
-Array<T>& Array<T>::unshift(std::initializer_list<T> il)
-{
+template <class T> Array<T> &Array<T>::unshift(std::initializer_list<T> il) {
     data.insert(data.begin(), il);
     return *this;
 }
 
-template <class T>
-Array<T>& Array<T>::unshift(T e)
-{
+template <class T> Array<T> &Array<T>::unshift(T e) {
     data.insert(data.begin(), e);
     return *this;
 }
 
-template <class T>
-std::string Array<T>::join()
-{
-    return join(", ");
-}
+template <class T> std::string Array<T>::join() { return join(", "); }
 
-template <class T>
-std::string Array<T>::join(std::string spt)
-{
+template <class T> std::string Array<T>::join(std::string spt) {
     std::ostringstream res;
-    for (size_t i = 0; i < size() - 1; ++i)
-        res << data[i] << spt;
+    for (size_t i = 0; i < size() - 1; ++i) res << data[i] << spt;
     res << data[size() - 1];
     return res.str();
 }
 
-template <class T>
-std::ostream& Array<T>::output(std::ostream& os)
-{
+template <class T> std::ostream &Array<T>::output(std::ostream &os) {
     std::ostringstream res;
     res << "[" << join() << "]";
     os << res.str() << std::endl;
     return os;
 }
 
-template <class T>
-size_t Array<T>::size()
-{
-    return data.size();
-}
+template <class T> size_t Array<T>::size() { return data.size(); }
 
-template <class T>
-bool Array<T>::every(std::function<bool(T)> fn)
-{
-    for (T& el : data)
-        if (!fn(el))
-            return false;
+template <class T> bool Array<T>::every(std::function<bool(T)> fn) {
+    for (T &el : data)
+        if (!fn(el)) return false;
     return true;
 }
 
-template <class T>
-bool Array<T>::some(std::function<bool(T)> fn)
-{
-    for (T& el : data)
-        if (fn(el))
-            return true;
+template <class T> bool Array<T>::some(std::function<bool(T)> fn) {
+    for (T &el : data)
+        if (fn(el)) return true;
     return false;
 }
 
-template <class T>
-T* Array<T>::find(std::function<bool(T)> fn)
-{
-    for (T& el : data)
-        if (fn(el))
-            return &el;
+template <class T> T *Array<T>::find(std::function<bool(T)> fn) {
+    for (T &el : data)
+        if (fn(el)) return &el;
     return nullptr;
 }
 
-template <class T>
-int Array<T>::findIndex(std::function<bool(T)> fn)
-{
+template <class T> int Array<T>::findIndex(std::function<bool(T)> fn) {
     for (int i = 0; i < size(); ++i)
-        if (fn(data[i]))
-            return i;
+        if (fn(data[i])) return i;
     return -1;
 }
 
-template <class T>
-bool Array<T>::includes(T& target)
-{
-    for (T& el : data)
-        if (el == target)
-            return true;
+template <class T> bool Array<T>::includes(T &target) {
+    for (T &el : data)
+        if (el == target) return true;
     return false;
 }
 
-template <class T>
-bool Array<T>::includes(T&& target)
-{
-    return includes(target);
-}
+template <class T> bool Array<T>::includes(T &&target) { return includes(target); }
 
-template <class T>
-int Array<T>::indexOf(T& t)
-{
-    return indexOf(t, 0);
-}
+template <class T> int Array<T>::indexOf(T &t) { return indexOf(t, 0); }
 
-template <class T>
-int Array<T>::indexOf(T&& t)
-{
-    return indexOf(t);
-}
+template <class T> int Array<T>::indexOf(T &&t) { return indexOf(t); }
 
-template <class T>
-int Array<T>::indexOf(T& t, size_t b)
-{
+template <class T> int Array<T>::indexOf(T &t, size_t b) {
     for (int i = b; i < size(); ++i)
-        if (data[i] == t)
-            return i;
+        if (data[i] == t) return i;
     return -1;
 }
 
-template <class T>
-int Array<T>::indexOf(T&& t, size_t b)
-{
-    return indexOf(t, b);
-}
+template <class T> int Array<T>::indexOf(T &&t, size_t b) { return indexOf(t, b); }
 
-template <class T>
-int Array<T>::lastIndexOf(T& t)
-{
-    return lastIndexOf(t, size() - 1);
-}
+template <class T> int Array<T>::lastIndexOf(T &t) { return lastIndexOf(t, size() - 1); }
 
-template <class T>
-int Array<T>::lastIndexOf(T&& t)
-{
-    return lastIndexOf(t);
-}
+template <class T> int Array<T>::lastIndexOf(T &&t) { return lastIndexOf(t); }
 
-template <class T>
-int Array<T>::lastIndexOf(T& t, size_t b)
-{
+template <class T> int Array<T>::lastIndexOf(T &t, size_t b) {
     for (int i = b; i >= 0; --i)
-        if (data[i] == t)
-            return i;
+        if (data[i] == t) return i;
     return -1;
 }
 
-template <class T>
-int Array<T>::lastIndexOf(T&& t, size_t b)
-{
-    return lastIndexOf(t, b);
-}
+template <class T> int Array<T>::lastIndexOf(T &&t, size_t b) { return lastIndexOf(t, b); }
 
-template <class T>
-Array<T>& Array<T>::copyWithin(size_t target)
-{
+template <class T> Array<T> &Array<T>::copyWithin(size_t target) {
     return copyWithin(target, 0, size());
 }
 
-template <class T>
-Array<T>& Array<T>::copyWithin(size_t target, size_t start)
-{
+template <class T> Array<T> &Array<T>::copyWithin(size_t target, size_t start) {
     return copyWithin(target, start, size());
 }
 
-template <class T>
-Array<T>& Array<T>::copyWithin(size_t target, size_t start, size_t end)
-{
+template <class T> Array<T> &Array<T>::copyWithin(size_t target, size_t start, size_t end) {
     std::vector<T> tmp(data.begin() + start, data.begin() + end);
-    for (size_t i = 0, j = target; i < tmp.size() && j < end; ++i, ++j)
-        data[j] = tmp[i];
+    for (size_t i = 0, j = target; i < tmp.size() && j < end; ++i, ++j) data[j] = tmp[i];
     return *this;
 }
 
-template <class T>
-Array<T>& Array<T>::fill(T t)
-{
-    return fill(t, 0, size());
-}
+template <class T> Array<T> &Array<T>::fill(T t) { return fill(t, 0, size()); }
 
-template <class T>
-Array<T>& Array<T>::fill(T t, size_t start)
-{
-    return fill(t, start, size());
-}
+template <class T> Array<T> &Array<T>::fill(T t, size_t start) { return fill(t, start, size()); }
 
-template <class T>
-Array<T>& Array<T>::fill(T t, size_t start, size_t end)
-{
-    if (size() < end)
-        data.resize(end);
-    for (int i = start; i < end; ++i)
-        data[i] = t;
+template <class T> Array<T> &Array<T>::fill(T t, size_t start, size_t end) {
+    if (size() < end) data.resize(end);
+    for (int i = start; i < end; ++i) data[i] = t;
     return *this;
 }
 
-template <class T>
-Array<T>& Array<T>::reverse()
-{
+template <class T> Array<T> &Array<T>::reverse() {
     auto l = data.begin();
     auto r = data.end();
-    while ((l != r) && l != --r)
-    {
+    while ((l != r) && l != --r) {
         std::iter_swap(l, r);
         ++l;
     }
     return *this;
 }
 
-template <class T>
-Array<T>& Array<T>::sort()
-{
+template <class T> Array<T> &Array<T>::sort() {
     return sort([](const T l, const T r) { return (l < r); });
 }
 
-template <class T>
-Array<T>& Array<T>::sort(std::function<bool(T, T)> cmp)
-{
+template <class T> Array<T> &Array<T>::sort(std::function<bool(T, T)> cmp) {
     std::sort(data.begin(), data.end(), cmp);
     return *this;
 }
 
-template <class T>
-Array<T>& Array<T>::splice(size_t start)
-{
+template <class T> Array<T> &Array<T>::splice(size_t start) {
     return splice(start, size() - start, {});
 }
 
-template <class T>
-Array<T>& Array<T>::splice(size_t start, size_t deleteCount)
-{
+template <class T> Array<T> &Array<T>::splice(size_t start, size_t deleteCount) {
     return splice(start, deleteCount, {});
 }
 
 template <class T>
-Array<T>& Array<T>::splice(size_t start,
-                           size_t deleteCount,
-                           std::initializer_list<T> il)
-{
+Array<T> &Array<T>::splice(size_t start, size_t deleteCount, std::initializer_list<T> il) {
     size_t l = il.size();
 
-    if (l > 0)
-        data.insert(data.begin() + start, il);
+    if (l > 0) data.insert(data.begin() + start, il);
 
-    data.erase(data.begin() + start + l,
-               data.begin() + start + l + deleteCount);
+    data.erase(data.begin() + start + l, data.begin() + start + l + deleteCount);
     return *this;
 }
 
-template <class T>
-Array<T> Array<T>::from()
-{
+template <class T> Array<T> Array<T>::from() {
     Array<T> tmp(*this);
     return std::move(tmp);
 }
 
-template <class T>
-Array<T> Array<T>::concat(Array<T>& B)
-{
+template <class T> Array<T> Array<T>::concat(Array<T> &B) {
     Array tmp(*this);
     tmp.data.insert(tmp.data.end(), B.data.begin(), B.data.end());
     return std::move(tmp);
 }
 
-template <class T>
-Array<T> Array<T>::filter(std::function<bool(T)> fn)
-{
+template <class T> Array<T> Array<T>::filter(std::function<bool(T)> fn) {
     Array<T> tmp;
     for (auto el : data)
-        if (fn(el))
-            tmp.push(el);
+        if (fn(el)) tmp.push(el);
     return std::move(tmp);
 }
 
-template <class T>
-template <class U>
-Array<U> Array<T>::map(std::function<U(T)> fn)
-{
+template <class T> template <class U> Array<U> Array<T>::map(std::function<U(T)> fn) {
     Array<U> tmp;
-    for (auto el : data)
-        tmp.push(fn(el));
+    for (auto el : data) tmp.push(fn(el));
     return std::move(tmp);
 }
 
-template <class T>
-Array<T> Array<T>::slice()
-{
-    return slice(0, size());
-}
+template <class T> Array<T> Array<T>::slice() { return slice(0, size()); }
 
-template <class T>
-Array<T> Array<T>::slice(size_t start)
-{
-    return slice(start, size());
-}
+template <class T> Array<T> Array<T>::slice(size_t start) { return slice(start, size()); }
 
-template <class T>
-Array<T> Array<T>::slice(size_t start, size_t end)
-{
+template <class T> Array<T> Array<T>::slice(size_t start, size_t end) {
     iterator l = data.begin() + start;
     iterator r = data.begin() + end;
     Array<T> tmp(l, r);
     return std::move(tmp);
 }
 
-template <class T>
-template <class U>
-U Array<T>::reduce(std::function<U(U, T)> fn, U init_value)
-{
+template <class T> template <class U> U Array<T>::reduce(std::function<U(U, T)> fn, U init_value) {
     U c = init_value;
-    for (auto el : data)
-        c = fn(c, el);
+    for (auto el : data) c = fn(c, el);
     return std::move(c);
 }
 
 template <class T>
 template <class U>
-U Array<T>::reduceRight(std::function<U(U, T)> fn, U init_value)
-{
+U Array<T>::reduceRight(std::function<U(U, T)> fn, U init_value) {
     U c = init_value;
-    for (int i = size() - 1; i >= 0; --i)
-        c = fn(c, data[i]);
+    for (int i = size() - 1; i >= 0; --i) c = fn(c, data[i]);
     return std::move(c);
 }
 
-template <class T>
-void Array<T>::forEach(std::function<void(T)> fn)
-{
-    for (auto el : data)
-        fn(el);
+template <class T> void Array<T>::forEach(std::function<void(T)> fn) {
+    for (auto el : data) fn(el);
 }
--- Array/test/test_Array.cc
+++ Array/test/test_Array.cc
@@ -6,36 +6,24 @@
 using std::string;
 using std::vector;
 
-Array<int> one()
-{
-    return std::move(Array<int>{1, 2, 3, 4, 5});
-}
+Array<int> one() { return std::move(Array<int>{1, 2, 3, 4, 5}); }
 
-TEST_CASE("Constructor::Fill")
-{
-    REQUIRE(Array<int>(5, 1).data == vector<int>{1, 1, 1, 1, 1});
-}
+TEST_CASE("Constructor::Fill") { REQUIRE(Array<int>(5, 1).data == vector<int>{1, 1, 1, 1, 1}); }
 
-TEST_CASE("Constructor::init list")
-{
-    REQUIRE(one().data == vector<int>{1, 2, 3, 4, 5});
-}
+TEST_CASE("Constructor::init list") { REQUIRE(one().data == vector<int>{1, 2, 3, 4, 5}); }
 
-TEST_CASE("Constructor::vector")
-{
+TEST_CASE("Constructor::vector") {
     vector<int> a{1, 2, 3, 4, 5};
     REQUIRE(Array<int>(a).data == a);
 }
 
-TEST_CASE("Constructor::vector iterator")
-{
+TEST_CASE("Constructor::vector iterator") {
     vector<int> a{1, 2, 3, 4, 5};
-    REQUIRE(Array<int>(a.begin() + 1, a.begin() + 4).data ==
-            vector<int>(a.begin() + 1, a.begin() + 4));
+    REQUIRE(
+        Array<int>(a.begin() + 1, a.begin() + 4).data == vector<int>(a.begin() + 1, a.begin() + 4));
 }
 
-TEST_CASE("Operator::[]")
-{
+TEST_CASE("Operator::[]") {
     auto a = one();
     REQUIRE(a[0] == 1);
     REQUIRE(a[4] == 5);
@@ -43,8 +31,7 @@ TEST_CASE("Operator::[]")
     REQUIRE(a[3] == 666);
 }
 
-TEST_CASE("Method::pop/push/shift/unshift")
-{
+TEST_CASE("Method::pop/push/shift/unshift") {
     Array<int> a{0};
     REQUIRE(a.size() == 1);
     REQUIRE(a.push(1).data == vector<int>{0, 1});
@@ -53,9 +40,8 @@ TEST_CASE("Method::pop/push/shift/unshift")
     REQUIRE(a.shift() == 2);
 }
 
-TEST_CASE("Method::every/some/find/findIndex")
-{
-    auto a = one();
+TEST_CASE("Method::every/some/find/findIndex") {
+    auto a    = one();
     auto cond = [](int el) { return !(el % 2); };
     REQUIRE_FALSE(a.every(cond));
     REQUIRE(*(a.find(cond)) == 2);
@@ -63,10 +49,9 @@ TEST_CASE("Method::every/some/find/findIndex")
     REQUIRE(a.findIndex(cond) == 1);
 }
 
-TEST_CASE("Method::includes")
-{
+TEST_CASE("Method::includes") {
     auto a = one();
-    int e = 4;
+    int e  = 4;
     int ee = 6;
     REQUIRE(a.includes(e));
     REQUIRE(a.includes(4));
@@ -74,9 +59,8 @@ TEST_CASE("Method::includes")
     REQUIRE_FALSE(a.includes(6));
 }
 
-TEST_CASE("Method::indexOf/lastIndexOf")
-{
-    auto a = one();  // [1,2,3,4,5]
+TEST_CASE("Method::indexOf/lastIndexOf") {
+    auto a = one(); // [1,2,3,4,5]
     int el = 6;
     REQUIRE(a.indexOf(3) == 2);
     REQUIRE(a.indexOf(3, 3) == -1);
@@ -89,123 +73,101 @@ TEST_CASE("Method::indexOf/lastIndexOf")
     REQUIRE(a.lastIndexOf(6) == -1);
 }
 
-TEST_CASE("Method::copyWithin")
-{
+TEST_CASE("Method::copyWithin") {
     Array<char> a{'a', 'b', 'c', 'd', 'e'};
-    REQUIRE(a.copyWithin(0, 3, 4).data ==
-            vector<char>{'d', 'b', 'c', 'd', 'e'});
+    REQUIRE(a.copyWithin(0, 3, 4).data == vector<char>{'d', 'b', 'c', 'd', 'e'});
     REQUIRE(a.copyWithin(1, 3).data == vector<char>{'d', 'd', 'e', 'd', 'e'});
 }
 
-TEST_CASE("Method::fill")
-{
+TEST_CASE("Method::fill") {
     auto a = one();
     REQUIRE(a.fill(-1, 2, 4).data == vector<int>{1, 2, -1, -1, 5});
     REQUIRE(a.fill(-2, 3).data == vector<int>{1, 2, -1, -2, -2});
     REQUIRE(a.fill(-3).data == vector<int>{-3, -3, -3, -3, -3});
 }
 
-TEST_CASE("Method::reverse")
-{
-    REQUIRE(one().reverse().data == vector<int>{5, 4, 3, 2, 1});
-}
+TEST_CASE("Method::reverse") { REQUIRE(one().reverse().data == vector<int>{5, 4, 3, 2, 1}); }
 
-TEST_CASE("Method::sort")
-{
+TEST_CASE("Method::sort") {
     Array<int> a{1, 1, 4, 5, 1, 4};
     REQUIRE(a.sort().data == vector<int>{1, 1, 1, 4, 4, 5});
-    REQUIRE(a.sort([](int l, int r) { return 1.0 / l < 1.0 / r; }).data ==
-            vector<int>{5, 4, 4, 1, 1, 1});
+    REQUIRE(a.sort([](int l, int r) {
+                 return 1.0 / l < 1.0 / r;
+             }).data == vector<int>{5, 4, 4, 1, 1, 1});
 }
 
-TEST_CASE("Method::splice")
-{
+TEST_CASE("Method::splice") {
     Array<string> a{"angel", "clown", "mandarin", "sturgeon"};
 
-    REQUIRE(a.splice(2, 0, {"drum"}).data ==
-            vector<string>{"angel", "clown", "drum", "mandarin", "sturgeon"});
+    REQUIRE(
+        a.splice(2, 0, {"drum"}).data ==
+        vector<string>{"angel", "clown", "drum", "mandarin", "sturgeon"});
 
-    REQUIRE(a.splice(3, 1).data ==
-            vector<string>{"angel", "clown", "drum", "sturgeon"});
+    REQUIRE(a.splice(3, 1).data == vector<string>{"angel", "clown", "drum", "sturgeon"});
 
-    REQUIRE(a.splice(2, 1, {"trumpet"}).data ==
-            vector<string>{"angel", "clown", "trumpet", "sturgeon"});
+    REQUIRE(
+        a.splice(2, 1, {"trumpet"}).data ==
+        vector<string>{"angel", "clown", "trumpet", "sturgeon"});
 
-    REQUIRE(a.splice(0, 2, {"parrot", "anemone", "blue"}).data ==
-            vector<string>{"parrot", "anemone", "blue", "trumpet", "sturgeon"});
+    REQUIRE(
+        a.splice(0, 2, {"parrot", "anemone", "blue"}).data ==
+        vector<string>{"parrot", "anemone", "blue", "trumpet", "sturgeon"});
 
-    REQUIRE(a.splice(a.size() - 3, 2).data ==
-            vector<string>{"parrot", "anemone", "sturgeon"});
+    REQUIRE(a.splice(a.size() - 3, 2).data == vector<string>{"parrot", "anemone", "sturgeon"});
 
     REQUIRE(a.splice(1).data == vector<string>{"parrot"});
 }
 
-TEST_CASE("Method::from")
-{
-    REQUIRE(one().from().data == one().data);
-}
+TEST_CASE("Method::from") { REQUIRE(one().from().data == one().data); }
 
-TEST_CASE("Method::concat")
-{
+TEST_CASE("Method::concat") {
     Array<int> a{1, 2, 3};
     Array<int> b{4, 5, 6};
     Array<int> c{7, 8, 9};
-    REQUIRE(a.concat(b).concat(c).data ==
-            vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9});
+    REQUIRE(a.concat(b).concat(c).data == vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9});
 }
 
-TEST_CASE("Method::filter")
-{
-    Array<string> a{"spray",     "limit",       "elite",
-                    "exuberant", "destruction", "present"};
+TEST_CASE("Method::filter") {
+    Array<string> a{"spray", "limit", "elite", "exuberant", "destruction", "present"};
     auto flt = [](const string s) { return s.length() > 6; };
     auto res = a.filter(flt);
     REQUIRE(res.data == vector<string>{"exuberant", "destruction", "present"});
 }
 
-TEST_CASE("Method::map")
-{
+TEST_CASE("Method::map") {
     auto mpr = [](int a) { return a * a; };
     auto res = one().map<int>(mpr);
     REQUIRE(res.data == vector<int>{1, 4, 9, 16, 25});
 }
 
-TEST_CASE("Method::slice")
-{
+TEST_CASE("Method::slice") {
     Array<string> a{"ant", "bison", "camel", "duck", "elephant"};
     REQUIRE(a.slice(2).data == vector<string>{"camel", "duck", "elephant"});
     REQUIRE(a.slice(2, 4).data == vector<string>{"camel", "duck"});
-    REQUIRE(a.slice(1, 5).data ==
-            vector<string>{"bison", "camel", "duck", "elephant"});
+    REQUIRE(a.slice(1, 5).data == vector<string>{"bison", "camel", "duck", "elephant"});
 }
-TEST_CASE("Method::reduce")
-{
+TEST_CASE("Method::reduce") {
     Array<int> a{1, 2, 3, 4};
     auto reducer = [](int acc, int cur) -> int { return acc + cur; };
     REQUIRE(a.reduce<int>(reducer, 0) == 10);
     REQUIRE(a.reduce<int>(reducer, 5) == 15);
 }
 
-TEST_CASE("Method::reduceRight")
-{
+TEST_CASE("Method::reduceRight") {
     Array<Array<int>> a{{0, 1}, {2, 3}, {4, 5}};
-    auto rightReducer = [](Array<int> acc, Array<int> cur) {
-        return acc.concat(cur);
-    };
-    auto res = a.reduceRight<Array<int>>(rightReducer, Array<int>{});
+    auto rightReducer = [](Array<int> acc, Array<int> cur) { return acc.concat(cur); };
+    auto res          = a.reduceRight<Array<int>>(rightReducer, Array<int>{});
     REQUIRE(res.data == vector<int>{4, 5, 2, 3, 0, 1});
 }
 
-TEST_CASE("Method::forEach")
-{
+TEST_CASE("Method::forEach") {
     auto a = one();
     vector<int> res;
     a.forEach([&res](int a) { res.push_back(a * 2); });
     REQUIRE(res == vector<int>{2, 4, 6, 8, 10});
 }
 
-TEST_CASE("Method::join")
-{
+TEST_CASE("Method::join") {
     REQUIRE(one().join() == "1, 2, 3, 4, 5");
     REQUIRE(one().join("") == "12345");
     REQUIRE(one().join("-") == "1-2-3-4-5");
